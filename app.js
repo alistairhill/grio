ff507c5ef596a758e76455ff8270826a096c99c1
Alistair <plantonly@gmail.com>
2016-05-20 08:37:59 -0700

    got first file

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
   'use strict';
 
   angular.module('wordFinder', [])
-  .controller('mainController', function($scope) {
+  .controller('mainController', function($scope, $http) {
     $scope.test = "test";
-  })
 
+    var fileName = "0608271h.html";
+    $http.get("../grio/data/" + fileName)
+    .success(function (response) {
+      console.log(response)
+    })
+    .error(function (response) {
+      console.error(response);
+    })
+
+  })
 })();
