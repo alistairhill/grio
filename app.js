9e51e2f33a2320d3e43e497f04aebcb456c5c8cd
Alistair <plantonly@gmail.com>
2016-05-20 09:06:33 -0700

    getting multiple files

--- app.js
+++ app.js
@@ -5,14 +5,25 @@
   .controller('mainController', function($scope, $http) {
     $scope.test = "test";
 
-    var fileName = "0608271h.html";
-    $http.get("../grio/data/" + fileName)
-    .success(function (response) {
-      console.log(response)
-    })
-    .error(function (response) {
-      console.error(response);
-    })
+    function getFilesNames() {
+      var fileNames = ["0608271h.html", "w00001.html", "w00004.html"];
+      return fileNames;
+    }
 
-  })
+    function getFiles() {
+      var fileNames = getFilesNames();
+
+      for (var i = 0; i < fileNames.length; i++) {
+        var fileName = fileNames[i];
+        $http.get("../grio/data/" + fileName)
+        .success(function (response) {
+          return response;
+        })
+        .error(function (response) {
+          console.error(response);
+        })
+      }
+    }
+
+  });
 })();
