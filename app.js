6b497a860ce05e8195ff106a07ba095719425cf5
Alistair <plantonly@gmail.com>
2016-05-20 10:45:15 -0700

    using service to get multi files

--- app.js
+++ app.js
@@ -2,28 +2,48 @@
   'use strict';
 
   angular.module('wordFinder', [])
-  .controller('mainController', function($scope, $http) {
+  .controller('mainController', function($scope, $q, $http, myServices) {
+    var books = [];
     $scope.test = "test";
 
-    function getFilesNames() {
-      var fileNames = ["0608271h.html", "w00001.html", "w00004.html"];
-      return fileNames;
+    activate();
+    function activate() {
+
+      var promises = [
+          myServices.getFiles("0608271h.html"),
+          myServices.getFiles("w00001.html"),
+          myServices.getFiles("w00004.html")
+      ];
+      $q.all(promises).then(function successHandler(results) {
+        var index = 0;
+        for (var i = 0; i < results.length; i++) {
+          books.push(results[i]);
+        }
+        console.log(books);
+      }, epicfail);
+
     }
 
-    function getFiles() {
-      var fileNames = getFilesNames();
-
-      for (var i = 0; i < fileNames.length; i++) {
-        var fileName = fileNames[i];
-        $http.get("../grio/data/" + fileName)
-        .success(function (response) {
-          return response;
-        })
-        .error(function (response) {
-          console.error(response);
-        })
-      }
+    function epicfail(response) {
+      console.error(response)
     }
 
+  })
+  .service('myServices', function($q, $http) {
+
+    this.getFiles = function(file) {
+      var deferred = $q.defer(),
+      config = {
+        method: 'get',
+        url: "../grio/data/" + file
+      };
+      $http(config)
+        .success(function(data) {
+          deferred.resolve([data, file]);
+        }).error(function(data) {
+          deferred.reject(data);
+        });
+      return deferred.promise;
+    };
   });
 })();
